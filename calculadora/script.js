3b86d5ee24301dbc5d88eb13b46458e234b46b73
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-05-24 12:35:08 -0300

    Projeto de uma calculadora

---
+++ calculadora/script.js
@@ -0,0 +1,22 @@
+const tela = document.getElementById("tela-display");
+let total = 0
+
+function adicionar(num){
+    if (tela.innerHTML.length == 15){
+        window.alert("nao pode")
+    }else{
+        tela.innerHTML += num;
+    }
+}
+
+function clean(){
+    tela.innerHTML = '';
+}
+
+function calculo(operator){
+    switch(operator){
+        case 1: {
+            
+        }
+    }
+}
